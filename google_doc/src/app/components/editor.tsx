26122deb0a90b13eec303e59c4debf75b4292196
Tianci Hu <321509302@qq.com>
2025-06-23 22:10:22 +0100

    add list,table and image extension

--- google_doc/src/app/components/editor.tsx
+++ google_doc/src/app/components/editor.tsx
@@ -1,7 +1,15 @@
 'use client'
 
+import Image from '@tiptap/extension-image'
+import Table from '@tiptap/extension-table'
+import TableCell from '@tiptap/extension-table-cell'
+import TableHeader from '@tiptap/extension-table-header'
+import TableRow from '@tiptap/extension-table-row'
+import TaskItem from '@tiptap/extension-task-item'
+import TaskList from '@tiptap/extension-task-list'
 import { EditorContent, useEditor } from '@tiptap/react'
 import StarterKit from '@tiptap/starter-kit'
+import ImageResize from 'tiptap-extension-resize-image'
 
 const Tiptap = () => {
   const editor = useEditor({
@@ -13,8 +21,39 @@ const Tiptap = () => {
           'focus:outline-none bg-white border border-[#C7C7C7] flex flex-col min-h-[1053px] w-[816px] pt-10 pr-15 pb-10 cursor-text ',
       },
     },
-    extensions: [StarterKit],
-    content: '<p>Hello World! 🌎️</p>',
+    extensions: [
+      StarterKit,
+      Image,
+      ImageResize,
+
+      TaskItem.configure({
+        nested: true,
+      }),
+      TaskList,
+
+      Table.configure({
+        resizable: true,
+      }),
+
+      TableRow,
+      TableHeader,
+      TableCell,
+    ],
+    content: `
+        <table>
+          <tbody>
+            <tr>
+              <th>Name</th>
+              <th colspan="3">Description</th>
+            </tr>
+            <tr>
+              <td>Cyndi Lauper</td>
+              <td>Singer</td>
+              <td>Songwriter</td>
+              <td>Actress</td>
+            </tr>
+          </tbody>
+        </table>`,
   })
   return (
     <div className='size-full overflow-x-auto bg-[#F9FBFD] px-4 print:px-0 print:bg-white print:overflow-visible'>
